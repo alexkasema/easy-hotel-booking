f4821825fd890bc325747d9029591fbcfd8920d5
AlexKasema <lexxmulwa@gmail.com>
2024-04-09 01:48:40 +0300

    Implemented register functionality in the backend by creating the user model and users route for the register endpoint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,9 @@ import cors from 'cors';
 import "dotenv/config";
 
 import mongoose from 'mongoose';
+import userRoutes from './routes/users';
+
+
 mongoose.connect(process.env.MONGO_URI as string);
 
 const app = express();
@@ -11,9 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors())
 
-app.get('/api/test', async (req: Request, res: Response) => {
-    res.json({message: 'Hello from express!'})
-});
+app.use("/api/users", userRoutes);
 
 app.listen(7000, () => {
     console.log('Server is running on port 7000');
