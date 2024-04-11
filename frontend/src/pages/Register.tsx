27aa6531c1f1136f03a0c00d989fe50ac32d8b6e
AlexKasema <lexxmulwa@gmail.com>
2024-04-11 13:42:43 +0300

    Implemented SignIn and SignOut functionality

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
 import { useNavigate } from "react-router-dom";
@@ -13,7 +13,7 @@ export type RegisterFormData = {
   };
 
 const Register = () => {
-    
+    const queryClient = useQueryClient();
     const navigate = useNavigate();
     const { showToast } = useAppContext();
     const {
@@ -26,7 +26,7 @@ const Register = () => {
     const mutation = useMutation(apiClient.register, {
     onSuccess: async () => {
         showToast({ message: "Registration Success!", type: "SUCCESS" });
-        // await queryClient.invalidateQueries("validateToken");
+        await queryClient.invalidateQueries("validateToken");
         navigate("/");   
     },
     onError: (error: Error) => {
