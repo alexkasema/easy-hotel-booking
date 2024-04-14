28af9f05ce29b2517f941b2301e08504b77a0a77
AlexKasema <lexxmulwa@gmail.com>
2024-04-15 01:46:05 +0300

    Implemented hotel booking via stripe and created an e2e test for the booking feature

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -2,19 +2,24 @@ import React, { useContext, useState } from "react";
 import { useQuery } from "react-query";
 import * as apiClient from '../api-client';
 import Toast from "../components/Toast";
+import { loadStripe, Stripe } from "@stripe/stripe-js";
+
+const STRIPE_PUB_KEY = import.meta.env.VITE_STRIPE_PUB_KEY || "";
 
 type ToastMessage = {
-    message: string;
+  message: string;
     type: "SUCCESS" | "ERROR";
-  };
+};
   
-  type AppContext = {
-    showToast: (toastMessage: ToastMessage) => void;
-    isLoggedIn: boolean;
-    // stripePromise: Promise<Stripe | null>;
-  };
+type AppContext = {
+  showToast: (toastMessage: ToastMessage) => void;
+  isLoggedIn: boolean;
+  stripePromise: Promise<Stripe | null>;
+};
 
   const AppContext = React.createContext<AppContext | undefined>(undefined);
+
+  const stripePromise = loadStripe(STRIPE_PUB_KEY);
 
   export const AppContextProvider = ({
     children,
@@ -34,6 +39,7 @@ type ToastMessage = {
             setToast(toastMessage); 
           },
           isLoggedIn: !isError,
+          stripePromise,
         }}
       >
         {toast && (
