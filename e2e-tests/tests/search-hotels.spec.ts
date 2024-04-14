88b8a8219680b5b741a1456973cac55ffcaad4fb
AlexKasema <lexxmulwa@gmail.com>
2024-04-15 02:37:24 +0300

    Created a my-bookings page where a user can view their hotel bookings. Created an e2e test for the page as well.

--- e2e-tests/tests/search-hotels.spec.ts
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -68,6 +68,6 @@ test("should book hotel", async ({ page }) => {
     await page.getByRole("button", { name: "Confirm Booking" }).click();
     await expect(page.getByText("Booking Saved!")).toBeVisible();
   
-    // await page.getByRole("link", { name: "My Bookings" }).click();
-    // await expect(page.getByText("Test Hotel UPDATED")).toBeVisible();
+    await page.getByRole("link", { name: "My Bookings" }).click();
+    await expect(page.getByText("Test Hotel UPDATED")).toBeVisible();
   });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ import EditHotel from "./pages/EditHotel";
 import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import Booking from "./pages/Booking";
+import MyBookings from "./pages/MyBookings";
 
 function App() {
   const { isLoggedIn } = useAppContext();
@@ -30,8 +31,9 @@ function App() {
             <>
               <Route path="/hotel/:hotelId/booking" element={<Layout> <Booking /> </Layout>} />
               <Route path="/add-hotel" element={<Layout> <AddHotel /> </Layout>} />
-              <Route path="/my-hotels" element={<Layout> <MyHotels /> </Layout>} />
               <Route path="/edit-hotel/:hotelId" element={<Layout> <EditHotel /> </Layout>} />
+              <Route path="/my-hotels" element={<Layout> <MyHotels /> </Layout>} />
+              <Route path="/my-bookings" element={<Layout> <MyBookings /> </Layout>} />             
             </>
           )}
           <Route path="*" element={<Navigate to="/" />} />
