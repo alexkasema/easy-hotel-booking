62794658144509ed06f096687f0dbcd97c863be3
AlexKasema <lexxmulwa@gmail.com>
2024-04-14 16:32:19 +0300

    Created the hotel details page for when a user clicks on a hotel and created an e2e test for the page

--- e2e-tests/tests/search-hotels.spec.ts
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -27,4 +27,15 @@ test("should show hotel search results", async ({ page }) => {
   
     await expect(page.getByText("Hotels found in Test City")).toBeVisible();
     await expect(page.getByText("Test Hotel UPDATED")).toBeVisible();
-  });
+});
+
+test("should show hotel detail", async ({ page }) => {
+    await page.goto(UI_URL);
+
+    await page.getByPlaceholder("Where are you going?").fill("Test City");
+    await page.getByRole("button", { name: "Search" }).click();
+
+    await page.getByText("Test Hotel UPDATED").click();
+    await expect(page).toHaveURL(/detail/);
+    await expect(page.getByRole("button", { name: "Book now" })).toBeVisible();
+});
