8224649982ae27ff0509bd647afb83e297111a4d
AlexKasema <lexxmulwa@gmail.com>
2024-04-15 03:07:39 +0300

    Created the home page of the app where The latest hotels are displayed.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,6 +15,7 @@ import Search from "./pages/Search";
 import Detail from "./pages/Detail";
 import Booking from "./pages/Booking";
 import MyBookings from "./pages/MyBookings";
+import Home from "./pages/Home";
 
 function App() {
   const { isLoggedIn } = useAppContext();
@@ -22,7 +23,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Layout><p>Home Page</p></Layout>} />
+          <Route path="/" element={<Layout> <Home /> </Layout>} />
           <Route path="/search" element={<Layout> <Search /> </Layout>} />
           <Route path="/detail/:hotelId" element={<Layout> <Detail /> </Layout>} />
           <Route path="/register" element={<Layout> <Register /> </Layout>} />
